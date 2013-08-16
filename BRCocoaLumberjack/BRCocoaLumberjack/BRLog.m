a4412d09207c13555d5c4d0eac8d140c079490b1
Matt Magoffin <git+matt@msqr.us>
2013-08-16 17:28:42 +1200

    Fix license in comments.

--- BRCocoaLumberjack/BRCocoaLumberjack/BRLog.m
+++ BRCocoaLumberjack/BRCocoaLumberjack/BRLog.m
@@ -3,7 +3,7 @@
 //  BRCocoaLumberjack
 //
 //  Created by Matt on 8/16/13.
-//  Copyright (c) 2013 Blue Rocket, Inc. All rights reserved.
+//  Copyright (c) 2013 Blue Rocket, Inc. Distributable under the terms of the Apache License, Version 2.0.
 //
 
 #import "BRLog.h"
